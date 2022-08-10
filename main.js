0b5ad9caa1bc476cd5f5d71c45d5ff005c2609f9
Rudra Shekhar Nag <82301307+Rudra-2009@users.noreply.github.com>
2022-08-10 19:46:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+function preLoad() {
+
+}
+function setup() {
+ canvas=createCanvas(400,400);
+ canvas.center();
+ video=createCapture(VIDEO);
+ video.size(400,400);
+ video.hide();
+ poseNet=ml5.poseNet(video,modelLoaded);
+ poseNet.on('pose',gotPoses);
+}
+function draw() {
+ image(video,0,0,400,400);
+ fill(255,0,0);
+ stroke(255,0,0);
+ circle(noseX,noseY,20);
+}
+function takeSnapshot() {
+ save('download.png');
+}
+function modelLoaded() {
+    console.log('PoseNet is Initialised');
+}
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x =  "+noseX);
+        console.log("nose y =  "+noseY);    
+    }
+}
